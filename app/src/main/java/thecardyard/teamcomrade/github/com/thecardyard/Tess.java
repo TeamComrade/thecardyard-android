0c64964c182c8215aef40606cc7c2118a282eeb1
w0414043 <w0414043@nscc.ca>
2018-09-25 20:20:20 -0300

    Add Some extra Implementations

--- app/src/main/java/thecardyard/teamcomrade/github/com/thecardyard/Tess.java
+++ app/src/main/java/thecardyard/teamcomrade/github/com/thecardyard/Tess.java
@@ -1,8 +1,25 @@
 package thecardyard.teamcomrade.github.com.thecardyard;
 import java.io.File;
-// import com.googlecode.tesseract.android.*;
+/*
+=== Tess-Two===
+
+tess-two Import structure
+import com.googlecode.tesseract.android.*;
+
+Java Documentation for tess-two
+https://rmtheis.github.io/tess-two/javadoc/index.html
+*/
+
+import com.googlecode.tesseract.android.TessBaseAPI;
 public class Tess implements Tesseract {
     public String executeOCR(File a){
+        //TODO Run this through various PSM's looking for best result
+        TessBaseAPI Tesseract = new TessBaseAPI();
+        
+        return null;
+    }
+    public String executeOCR(String Filename) {
+        //TODO Implement Overloaded OCR for extendable implementation
         return null;
     }
 }
--- app/src/main/java/thecardyard/teamcomrade/github/com/thecardyard/Tesseract.java
+++ app/src/main/java/thecardyard/teamcomrade/github/com/thecardyard/Tesseract.java
@@ -4,4 +4,6 @@ import java.io.File;
 public interface Tesseract {
 
     String executeOCR(File a);
+
+    String executeOCR(String Filename);
 }
