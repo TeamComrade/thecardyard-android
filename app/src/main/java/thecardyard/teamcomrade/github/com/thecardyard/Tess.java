f335f00108837040699c7591bb0c539b1ebb1216
w0414043 <w0414043@nscc.ca>
2018-09-25 19:13:37 -0300

    Begin tess-two Adding

---
+++ app/src/main/java/thecardyard/teamcomrade/github/com/thecardyard/Tess.java
@@ -0,0 +1,8 @@
+package thecardyard.teamcomrade.github.com.thecardyard;
+import java.io.File;
+
+public class Tess implements Tesseract {
+    public String executeOCR(File a){
+        return null;
+    }
+}
---
+++ app/src/main/java/thecardyard/teamcomrade/github/com/thecardyard/Tesseract.java
@@ -0,0 +1,7 @@
+package thecardyard.teamcomrade.github.com.thecardyard;
+import java.io.File;
+
+public interface Tesseract {
+
+    String executeOCR(File a);
+}
