717f46fe8c2954b7b9be2617e5fb139747f38df2
w0414043 <w0414043@nscc.ca>
2018-09-25 20:13:56 -0300

    Successfully Add Tess-two into the Gradle build process

--- app/src/main/java/thecardyard/teamcomrade/github/com/thecardyard/Tess.java
+++ app/src/main/java/thecardyard/teamcomrade/github/com/thecardyard/Tess.java
@@ -1,6 +1,6 @@
 package thecardyard.teamcomrade.github.com.thecardyard;
 import java.io.File;
-
+// import com.googlecode.tesseract.android.*;
 public class Tess implements Tesseract {
     public String executeOCR(File a){
         return null;
